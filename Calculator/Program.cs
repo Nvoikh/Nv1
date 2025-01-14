4ec63b25e5c99eec2eba3032e99b9c1234d637ae
Nvoikh <120413793+Nvoikh@users.noreply.github.com>
2025-01-14 23:12:49 +0300

    Add files via upload

---
+++ Calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Calculator
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите пожалуйста первое число:");
+            double sum1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("*,/,-,+ ?");
+            char symbol = Convert.ToChar(Console.ReadLine());
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            Console.WriteLine("Введите пожалуйста второе число:");
+            double sum2 = Convert.ToDouble(Console.ReadLine());
+            double result = 0;
+            bool isValid = true;
+            switch (symbol)
+            {
+                case '*': result = sum1 * sum2; break;
+                case '-': result = sum1 - sum2; break;
+                case '+': result = sum1 + sum2; break;
+                case '/': result = sum1 / sum2; break;
+            }
+            if (isValid)
+            {
+
+
+                Console.WriteLine($"Результатат: {result} ");
+                Console.WriteLine("Нажмите пробел для закрытия.");
+
+                Console.ReadKey();
+            }
+            
+            
+            
+        }
+    }
+}
