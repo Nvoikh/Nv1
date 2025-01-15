103505490bf71be444f83e3e0ebf4715feb45b5a
Nvoikh <120413793+Nvoikh@users.noreply.github.com>
2025-01-15 17:33:00 +0200

    version 0.2

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,30 +10,35 @@ namespace Calculator
     {
         static void Main()
         {
-            Console.WriteLine("Введите пожалуйста первое число:");
-            double sum1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("*,/,-,+ ?");
-            char symbol = Convert.ToChar(Console.ReadLine());
-            ConsoleKeyInfo keyInfo = Console.ReadKey();
-            Console.WriteLine("Введите пожалуйста второе число:");
-            double sum2 = Convert.ToDouble(Console.ReadLine());
-            double result = 0;
-            bool isValid = true;
-            switch (symbol)
-            {
-                case '*': result = sum1 * sum2; break;
-                case '-': result = sum1 - sum2; break;
-                case '+': result = sum1 + sum2; break;
-                case '/': result = sum1 / sum2; break;
-            }
-            if (isValid)
-            {
+              while (true)
+                {
+                Console.WriteLine("Введите пожалуйста первое число:");
+                double sum1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("*,/,-,+ ?");
+                char symbol = Convert.ToChar(Console.ReadLine());
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                Console.WriteLine("Введите пожалуйста второе число:");
+                double sum2 = Convert.ToDouble(Console.ReadLine());
+                double result = 0;
+                bool isValid = true;
+                switch (symbol)
+                {
+                    case '*': result = sum1 * sum2; break;
+                    case '-': result = sum1 - sum2; break;
+                    case '+': result = sum1 + sum2; break;
+                    case '/': result = sum1 / sum2; break;
+                }
+                if (isValid)
+                {
 
 
-                Console.WriteLine($"Результатат: {result} ");
-                Console.WriteLine("Нажмите пробел для закрытия.");
+                    Console.WriteLine($"Результатат: {result} ");
 
-                Console.ReadKey();
+                    Console.WriteLine("Выполнить решение опять? Нажмите l- что бы подтвердить.");
+                    if (Console.ReadLine() != "l")
+                        break;
+
+                }
             }
             
             
