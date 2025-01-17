df22e22c46bcf74fa024dd0b31f12871f6874716
Nvoikh <120413793+Nvoikh@users.noreply.github.com>
2025-01-17 18:43:04 +0200

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            numbers[0] = 10;
+            numbers[1] = 7;
+            numbers[2] = 2;
+            numbers[3] = 5;
+            numbers[4] = 9;
+            Array.Reverse(numbers);
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            string[] studentsNames = new string[10];
+            double[] studentsResults = new double[4]
+            {
+                5,6.5,10,11,
+            };
+        }
+    }
+}
