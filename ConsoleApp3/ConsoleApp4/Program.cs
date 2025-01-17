9f6816175eff50a2696b4efd07993be22a8e1789
Nvoikh <120413793+Nvoikh@users.noreply.github.com>
2025-01-17 17:05:37 +0200

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WeekDays.Friday);
+            Console.WriteLine((int)WeekDays.Thursday);
+
+            Console.WriteLine("GetName()");
+            Console.WriteLine(Enum.GetName(typeof(WeekDays),5));
+            Console.WriteLine("GetNames()");
+            foreach(string str in Enum.GetNames(typeof(WeekDays)))
+                Console.WriteLine(str);
+            Console.WriteLine("TryParse()");
+            WeekDays wdEnum;
+            Enum.TryParse<WeekDays>("1", out wdEnum);
+            Console.WriteLine(wdEnum);
+        }
+        enum WeekDays
+        {
+            Monday = 1,
+            Tuesday, 
+            Wednesday, 
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+            
+        }
+    }
+}
