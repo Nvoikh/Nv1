341427b5c2a585ab26cbcdb94b835b76a5248b35
Nvoikh <120413793+Nvoikh@users.noreply.github.com>
2025-01-17 22:12:59 +0200

    Add files via upload

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +10,24 @@ namespace ConsoleApp11
     {
         static void Main(string[] args)
         {
+            SortedList sl1 = new SortedList();
+            sl1.Add(3, "Number three");
+            sl1.Add(4, "Number four");
+            sl1.Add(1, "Number one");
+            sl1.Add(5, "Number five");
+            sl1.Add(2, "Number two");
+
+            sl1.RemoveAt(0);
+            
+            
+
+
+            for (int i = 0; i < sl1.Count; i++) {
+                Console.WriteLine($"Key: {sl1.GetKey(i)},Value: {sl1.GetByIndex(i)}");
+            }
+
         }
     }
+
+    
 }
