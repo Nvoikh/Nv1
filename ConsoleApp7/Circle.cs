160b1d02bced9186dc2779a27ff75d07e5f2a143
Nvoikh <120413793+Nvoikh@users.noreply.github.com>
2025-01-17 18:54:47 +0200

    Add files via upload

---
+++ ConsoleApp7/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    public class Circle
+    {
+        private const double PI = 3.14; //Pi
+        public double Radius { get; set; }
+        public Circle()
+        {
+            Console.WriteLine("Circle() created");
+        }
+        public Circle(double radius)
+            { this.Radius = radius; }
+        public double GetArea () => PI * Math.Pow(Radius, 2);
+        public double GetPerimeter() => 2 * PI * Radius;
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle();
+            c1.Radius = 5;
+            double a1 = c1.GetArea();
+            Console.WriteLine(a1);
+
+            Circle c2 = new Circle(5);
+            double a2 = c2.GetArea();
+            Console.WriteLine(a2);
+        }
+    }
+}
