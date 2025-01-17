dc5489cff6be3ef27b37821c2ab0bb6b588a03d7
Nvoikh <120413793+Nvoikh@users.noreply.github.com>
2025-01-17 18:34:02 +0200

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Hello ";
+            var hi = hello;
+            hello = "Privet";
+            Console.WriteLine(ReferenceEquals(hello, hi));
+
+            StringBuilder sb = new StringBuilder("this is string builder");
+            var sbNew = sb;
+            sb.Append("this is string builder");
+            Console.WriteLine(ReferenceEquals(sb, sbNew));
+
+            StringBuilder sbBuilder = new StringBuilder();
+            sbBuilder.Append("i have ");
+            sbBuilder.AppendFormat("{0:C}", 25);
+            sbBuilder.Insert(0, "John:");
+            sbBuilder.Remove(1, 2);
+            sbBuilder.Replace(" have ", "had");
+            Console.WriteLine(sbBuilder);
+        }
+    }
+}
