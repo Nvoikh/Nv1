2617a4a2970ba1b9d9f4db2985ab1e924f81b5c5
Nvoikh <120413793+Nvoikh@users.noreply.github.com>
2025-01-18 18:43:34 +0200

    Add files via upload

--- ConsoleApp17/Program.cs
+++ ConsoleApp17/Program.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace ConsoleApp17
 {
@@ -16,14 +17,38 @@ namespace ConsoleApp17
             StringDelegate stringDelegate = ToUpperCase;
            WriteOutput("This is a lower string", stringDelegate);
 
+            //events
+            Strings myString = new Strings();
+            myString.stringEvent += TextChanged;
+            string str;
+            do
+            {
+                str = Console.ReadLine();
+                if (!str.Equals("exit")) myString.Value = str;
+            } while (!str.Equals("exit"));
+
         }
+
         
         static void ToUpperCase(string text) => Console.WriteLine(text.ToUpper());
         static void ToLowerCase(string text) => Console.WriteLine(text.ToUpper());
+
+        static void TextChanged (string text) => Console.WriteLine($"You entered: {text}");
         static void WriteOutput(string text, StringDelegate stringDelegate)
         {
             Console.WriteLine($"Before: {text}");
             stringDelegate(text);
         }
+    }
+
+    public class Strings
+    {
+        private string stringValue;
+        public event StringDelegate stringEvent;
+
+        public string Value { set { 
+                this.stringValue = value;
+                this.stringEvent(this.stringValue);
+            } }
     }
 }
