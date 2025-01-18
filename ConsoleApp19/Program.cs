2acfa5cd1c3adca715c2c080e859579f2e253692
Nvoikh <120413793+Nvoikh@users.noreply.github.com>
2025-01-18 22:10:52 +0200

    Add files via upload

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            HelloClass hello = new HelloClass();
+            hello.HelloUniverse();
+            hello.HelloWorld();
+        }
+        public partial class HelloClass {
+            public void HelloWorld() => Console.WriteLine("Hello world!");
+        }
+            public partial class HelloClass {
+            public void HelloUniverse() => Console.WriteLine("Hello Petya");
+        }
+    }
+}
