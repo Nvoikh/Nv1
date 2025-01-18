2cd90b1a75f20f64327538c6fe888188586a8583
Nvoikh <120413793+Nvoikh@users.noreply.github.com>
2025-01-18 22:27:09 +0200

    Add files via upload

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<int> firstInt = null;
+            int? secondInt = -4;
+            int thirdInt = 32;
+            int forthInt = secondInt ?? 0;
+
+            Nullable<double> firstDouble = null;
+            double? secondDouble = null;
+            double thirdDouble = 12.24;
+            Console.WriteLine($"Integer => firstInt: {firstInt}, secondInt: {secondInt}, thirdInt: {thirdInt} , forthInt {forthInt}");
+            Console.WriteLine($"Integer => firstDouble: {firstDouble}, secondDouble: {secondDouble}, thirdDouble: {thirdDouble}");
+        }
+    }
+}
