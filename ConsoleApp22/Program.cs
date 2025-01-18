f8c524930cf0765171b75feb673671ab0377508d
Nvoikh <120413793+Nvoikh@users.noreply.github.com>
2025-01-18 22:53:40 +0200

    Add files via upload

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> IsEven = IsEven;
+            Console.WriteLine(IsEven(5));
+        }
+
+        static bool IsEven (int number) =>  number % 2 == 0;
+
+        
+
+        static bool IsLowerCase(string text) => text.Equals(text.ToLower());
+        
+    }
+}
