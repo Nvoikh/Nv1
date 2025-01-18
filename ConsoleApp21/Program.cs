1f93fb0eb67d4c1fe7fc8ef284d6b73f3680ee15
Nvoikh <120413793+Nvoikh@users.noreply.github.com>
2025-01-18 22:41:05 +0200

    Add files via upload

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double,double > square = delegate(double value) { return value * value; };
+            Console.WriteLine($"square = {square(2)}");
+
+            Func<double, double> cubic = (double value) => value * value * value; 
+            Console.WriteLine($"cubic = {cubic(2)}");
+
+            Func < double> two= Two;
+            Console.WriteLine($"two = {two()}");
+
+            
+        }
+
+        static double Square (double number) => Math.Pow (number, 2);
+
+        static double Cubic (double number ) => Math.Pow (number, 3);
+        static double Two() => 2;
+
+        static void Show() => Console.WriteLine("Hello There !");
+     }
+}
