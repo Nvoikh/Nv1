6536d6c09e3c9e9f3b81802598dd916292bc533d
Nvoikh <120413793+Nvoikh@users.noreply.github.com>
2025-01-18 17:48:53 +0200

    Add files via upload

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    public class SampleCollection
+    {
+        private string[] array = new string[60];
+
+        public string this[int i]
+        {
+            get { return array[i]; }
+            set { array[i] = value; }
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                SampleCollection sampleCollection = new SampleCollection();
+                sampleCollection[0] = "25";
+                Console.WriteLine($"The value = {sampleCollection[0]}");
+            }
+        }
+    }
+}
