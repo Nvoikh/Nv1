861511a573eb7563e8932784ce9c91d7ef76de0b
Nvoikh <120413793+Nvoikh@users.noreply.github.com>
2025-01-18 18:51:07 +0200

    Add files via upload

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test<string> firstTest = new Test<string>();
+            firstTest.Value = "This is a string ";
+            firstTest.Details();
+
+            Test<int> secondTest = new Test<int>();
+            secondTest.Value = 5;
+            secondTest.Details();
+
+       
+        }
+        public class Test<T>
+        {
+            public T Value { get; set; }
+            public void Details() => Console.WriteLine($"Parametr type: {typeof(T)}, value: {Value}");
+            
+        }
+    }
+}
