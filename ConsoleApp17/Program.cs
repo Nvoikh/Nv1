d5f66b165d08b99bc859c1c1684fd7303c7dd792
Nvoikh <120413793+Nvoikh@users.noreply.github.com>
2025-01-18 18:32:46 +0200

    Add files via upload

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    public delegate void StringDelegate(string text);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringDelegate stringDelegate = ToUpperCase;
+           WriteOutput("This is a lower string", stringDelegate);
+
+        }
+        
+        static void ToUpperCase(string text) => Console.WriteLine(text.ToUpper());
+        static void ToLowerCase(string text) => Console.WriteLine(text.ToUpper());
+        static void WriteOutput(string text, StringDelegate stringDelegate)
+        {
+            Console.WriteLine($"Before: {text}");
+            stringDelegate(text);
+        }
+    }
+}
